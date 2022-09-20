f1fb3892d3f997aa13e9f6012c581478af4720f0
jbfermor <jbfermor@hotmail.com>
2022-09-20 09:42:25 +0200

    help view added

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :comment_types
   resources :zones
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/help", to: "home#help", as: "help"
   # Defines the root path route ("/")
   root "comments#index"
 end
